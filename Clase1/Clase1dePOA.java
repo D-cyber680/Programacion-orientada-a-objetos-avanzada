b23dc43c4c12e27d7420a0929abe31d5f0efcf5b
DanielSantos <72686197+D-cyber680@users.noreply.github.com>
2021-02-17 13:18:51 -0800

    Update Clase1dePOA.java

--- Clase1/Clase1dePOA.java
+++ Clase1/Clase1dePOA.java
@@ -3,5 +3,6 @@ public class Clase1dePOA {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("New line");
+        System.out.println("Hola mundo");
     }
 }
